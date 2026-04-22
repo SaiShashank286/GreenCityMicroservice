5eb06a0becceeaf0485f6bef281e86253755d6ba
Sai Shashank <2478286@cognizant.com>
2026-04-22 14:14:25 +0530

    added service

---
+++ user-service/src/test/java/com/cognizant/greencity/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.greencity.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
